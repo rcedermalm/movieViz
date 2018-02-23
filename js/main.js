893f746c88ebf900e5f754f3035c532dec981551
Rebecca Cedermalm <rebce973@student.liu.se>
2018-02-23 10:23:27 +0100

    Added view

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ queue()
     .defer(d3.csv, 'data/IMDB/imdb_movie_metadata.csv')
     .await(ready);
 
-function ready(error, data){
-if(error) throw error;
+var view;
 
+function ready(error, data){
+  if(error) throw error;
+
+  view = new viewport(data);
   console.log("Got into the ready function.");
   console.log(data[1]);
 }
